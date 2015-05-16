d53eec081262de413b245b44c052df4f20f243cf
Adel Qalieh <aqalieh95@gmail.com>
2015-05-15 21:02:25 -0400

    Initial commit

---
+++ shtl.nim
@@ -0,0 +1,39 @@
+#
+#
+#           shtl - Nim app containers
+#        (c) Copyright 2015 Adel Qalieh
+#
+#    See the file "LICENSE", included in this
+#    distribution, for details about the copyright.
+#
+
+##   When the space shuttle's engines cut off, and you're finally in space, in
+##   orbit, weightless... I remember unstrapping from my seat, floating over to
+##   the window, and that's when I got my first view of Earth. Just a
+##   spectacular view, and a chance to see our planet as a planet.
+##   -- Sally Ride
+
+import parseopt2
+import docopt
+
+const doc = """
+shtl
+
+Usage:
+  shtl run <image>...
+
+Options:
+  -h --help     Show this screen.
+  -v --version  Show version.
+"""
+
+proc main() =
+  var images: seq[string] = @[]
+  let args = docopt(doc, version = "shtl 0.1.0")
+
+  if args["run"]:
+    echo("fly shuttle fly")
+    echo("images: ", args["<image>"])
+
+when isMainModule:
+  main()
---
+++ shtl.nimble
@@ -0,0 +1,10 @@
+[Package]
+name          = "shtl"
+version       = "0.1.0"
+author        = "Adel Qalieh"
+description   = "Nim app containers"
+license       = "GPLv3"
+bin           = "shtl"
+
+[Deps]
+Requires: "nim >= 0.10.0, docopt >= 0.5.0"
