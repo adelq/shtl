c66b48f858c60edbb578d96c9c08837a32673393
Adel Qalieh <aqalieh95@gmail.com>
2015-05-15 22:33:58 -0400

    Flesh out main function

--- shtl.nim
+++ shtl.nim
@@ -18,17 +18,35 @@ import docopt
 import nuuid
 import strutils
 import tables
+import logging
+import os
 
 const doc = """
 shtl
 
 Usage:
-  shtl run <image>...
+  shtl run <image>... [--volume=<vm>]... [--dir=<dir>]
 
 Options:
-  -h --help     Show this screen.
-  -v --version  Show version.
+  -h --help          Show this screen.
+  -v --version       Show version.
+  --volume=<vm>      Volumes to mount into the shared container environment.
+  --dir=<directory>  Directory in which to create container filesystem [default: ""].
 """
+
+proc genUID(): string =
+  return nuuid.generateUUID().toUpper()
+
+type volumeMap = Table[string, string]
+
+proc setvm(vm: var volumeMap, s:string) =
+  let elems = s.split(':')
+  if len(elems) != 2:
+    raise newException(ValueError, "volume must be of form key:path")
+  let key = elems[0]
+  if vm.hasKey(key):
+    raise newException(ValueError, "got multiple flags for volume " & key)
+  vm[key] = elems[1]
 
 proc main() =
   let args = docopt(doc, version = "shtl 0.1.0")
@@ -37,19 +55,25 @@ proc main() =
     echo("fly shuttle fly")
     echo("images: ", args["<image>"])
 
-proc genUID(): string =
-  return nuuid.generateUUID().toUpper()
+  var dir = $args["--dir"]
+  if dir == "":
+    info("--dir unset - using temporary directory")
+    dir = "shtl"
 
-type volumeMap = Table[string, string]
+  # Generating the Container Unique ID (UID)
+  let cuid = genUID()
 
-proc set(vm: var volumeMap, s:string) =
-  let elems = s.split(':')
-  if len(elems) != 2:
-    raise newException(ValueError, "volume must be of form key:path")
-  let key = elems[0]
-  if vm.hasKey(key):
-    raise newException(ValueError, "got multiple flags for volume " & key)
-  vm[key] = elems[1]
+  var volumes = initTable[string, string]()
+  for volume in @(args["--volume"]):
+    volumes.setvm(volume)
+
+  # Create a directory for the container
+  createDir(dir)
+
+  # Write the container document into the filesystem
+
+  # Copy the RAFs for each app into the stage2 directories
+  echo("Wrote filesystem to ", dir)
 
 when isMainModule:
   main()
